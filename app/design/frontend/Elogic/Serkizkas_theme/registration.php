28d34dc4e74db246d5d7acace3ea9e57b4e1859e
Serkizka <anastasiaserkiz@gmail.com>
2023-01-16 14:43:35 +0200

    move current filters over products cards

---
+++ app/design/frontend/Elogic/Serkizkas_theme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Elogic. All rights reserved.
+ * See https://elogic.co for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Elogic/Serkizkas_theme', __DIR__);
