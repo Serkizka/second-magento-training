60bfe404801196931cf2ceb13ab150d260347262
Nastasia_Serkiz <a.serkiz@elogic.com.ua>
2023-03-07 17:40:32 +0200

    start creating custom checkout module

---
+++ app/code/Serkizka/MyCheckout/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Serkizka_MyCheckout',
+    __DIR__
+);
--- app/design/frontend/Elogic/Serkizkas_theme/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © Elogic. All rights reserved.
- * See https://elogic.co for license details.
- */
-
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Elogic/Serkizkas_theme', __DIR__);
